7df64d62894236a90a5995789a3cf3f5ed9ab30f
Anders Kaplan (akn) <anders.kaplan@tobii.com>
2014-02-25 08:19:28 +0100

    ...

---
+++ Main.cpp
@@ -0,0 +1,6 @@
+// Main.cpp : Defines the entry point for the console application.
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
--- MiniMono.cpp
+++
@@ -1,6 +0,0 @@
-// MiniMono.cpp : Defines the entry point for the console application.
-
-int main(int argc, char* argv[])
-{
-	return 0;
-}
